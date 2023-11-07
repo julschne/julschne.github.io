3073d3938887ab6b22af94e779686868c98e0bbd
Julian Schneider <julian.schneider@varlix.io>
2023-11-07 23:18:15 +0100

    Updated project to vuetify

---
+++ nuxt.config.js
@@ -0,0 +1,12 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  css: ["vuetify/lib/styles/main.sass"],
+  build: {
+    transpile: ["vuetify"],
+  },
+  vite: {
+    base: "/",
+  },
+});
