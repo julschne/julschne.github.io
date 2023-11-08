16e7af40131587275c9afaac9c66612ee29d3771
Julian Schneider <julian.schneider@varlix.io>
2023-11-08 14:27:07 +0000

    Add hello world

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,11 @@
-<script setup lang="ts">
-const route = useRoute()
-</script>
-
 <template>
-  <div>
-    <h1>Nuxt Routing set up successfully!</h1>
-    <p>Current route: {{ route.path }}</p>
-    <a href="https://nuxt.com/docs/getting-started/routing" target="_blank">Learn more about Nuxt Routing</a>
-  </div>
+  <h1>Hello World!</h1>
 </template>
+
+<script>
+export default {
+  mounted() {
+    console.log("Component mounted")
+  }
+}
+</script>
